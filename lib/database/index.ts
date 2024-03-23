4652bcba42968120f01f33f3af7f3c27bc1460d6
Anfazjuwar <anfazjuwar@gmail.com>
2024-03-23 22:25:13 +0530

    new

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,26 +1,22 @@
 import mongoose from "mongoose";
 
-///manage databse connection
-
-const MongoDB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if (!MongoDB_URI) {
-    throw new Error("No Mongo DB URI provided");
-  }
+  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+
   cached.promise =
     cached.promise ||
-    mongoose.connect(MongoDB_URI, {
+    mongoose.connect(MONGODB_URI, {
       dbName: "evently",
       bufferCommands: false,
     });
+
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
-
-//Server actions
-//connectiondatabase()
